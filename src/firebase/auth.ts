45870284d82c9d98cec0a6c505339b90e4a46848
Yan Vieira <yanrei69@gmail.com>
2024-02-28 14:40:49 -0300

    feat: adiciona a função 'deleteCurrentUser()'

--- src/firebase/auth.ts
+++ src/firebase/auth.ts
@@ -11,7 +11,9 @@ import {
 
 /**
  * @param mode whether the function is being used in default mode or in an automatic test
+ * @param altAuth if the function is being used in an automatic test, it is necessary to pass the test ``auth`` instance
  * @param loginMethod one of the login methods described in ``Auth.loginMethod`` enum
+ * @returns an ``userCredential`` instance or the error code in case of failure
 */
 export async function createUser (mode:string, altAuth:Auth | null, loginMethod:string):Promise<UserCredential | string>
 export async function createUser (mode:string, altAuth:Auth | null, loginMethod:string, email:string, password:string):Promise<UserCredential | string>
@@ -52,4 +54,36 @@ export async function createUser (mode:string = FirebaseMode.default, altAuth:Au
     if (!cases[loginMethod]) return AuthEnum.Errors.invalidLoginMethod
 
     return cases[loginMethod](email, password)
+}
+
+
+/**
+ * Deletes the ``auth.currentUser`` and sign it out
+ * @param mode whether the function is being used in default mode or in an automatic test
+ * @param altAuth if the function is being used in an automatic test, it is necessary to pass the test ``auth`` instance
+ * @returns a string indicating the success or failure from the operation
+ */
+export async function deleteCurrentUser (mode:string):Promise<string>
+export async function deleteCurrentUser (mode:string, altAuth:Auth):Promise<string>
+export async function deleteCurrentUser (mode:string, altAuth?:Auth) {
+    const modeCases = {
+        'default': () => {return getFirebase().auth},
+        'autoTest': () => {return altAuth || getFirebase().auth}
+    } as {[key:string]: () => Auth}
+
+    const choseAuth = modeCases[mode]()
+
+    try {
+
+        if (!choseAuth.currentUser) return AuthEnum.Errors.noUserSignedIn
+
+        await deleteUser(choseAuth.currentUser)
+
+        if (choseAuth.currentUser) return 'failure'
+
+        return 'success'
+    } catch (error) {
+
+        return 'failure'
+    }
 }
