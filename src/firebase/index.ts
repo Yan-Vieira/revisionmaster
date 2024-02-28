5d7f9c7610f99693dac2e8b4ac76833ea551ce9c
Yan Vieira <yanrei69@gmail.com>
2024-02-28 11:07:00 -0300

    fix: adiciona o 'index.ts' do diretório '/firebase'

---
+++ src/firebase/index.ts
@@ -0,0 +1,11 @@
+import { getFirebase } from "./firebaseInit"
+import { createUser } from "./auth"
+
+/**
+ * Every functions that allow the communication between frontend and firebase services
+*/
+
+export  {
+    getFirebase,
+    createUser
+}
