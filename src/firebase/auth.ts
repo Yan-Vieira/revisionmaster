0ab7cade16b8e0aef936d207f044f5f2338b38d2
Yan Vieira <yanrei69@gmail.com>
2024-02-29 10:39:31 -0300

    feat: adiciona a função 'logOutCurrentUser()'

--- src/firebase/auth.ts
+++ src/firebase/auth.ts
@@ -7,6 +7,7 @@ import {
     signInWithPopup,
     GoogleAuthProvider,
     sendEmailVerification,
+    signOut,
     deleteUser
 } from "firebase/auth"
 
@@ -66,6 +67,36 @@ export async function createUser (mode:string = FirebaseMode.default, altAuth:Au
 
 
 /**
+ * @param mode whether the function is being used in default mode or in an automatic test
+ * @param altAuth if the function is being used in an automatic test, it is necessary to pass the test ``auth`` instance
+*/
+export async function logOutCurrentUser (mode:string):Promise<string>
+export async function logOutCurrentUser (mode:string, altAuth:Auth):Promise<string>
+export async function logOutCurrentUser (mode:string, altAuth?:Auth) {
+
+    const modeCases = {
+        'default': () => {return getFirebase().auth},
+        'autoTest': () => {return altAuth || getFirebase().auth}
+    } as {[key:string]: () => Auth}
+
+    const choseAuth = modeCases[mode]()
+
+    try {
+
+        if (!choseAuth.currentUser) return AuthEnum.Errors.noUserSignedIn
+
+        await signOut(choseAuth)
+
+        if (choseAuth.currentUser) return 'failure'
+
+        return 'success'
+    } catch (error) {
+
+        return (error as any).code as string
+    }
+}
+
+/**
  * Deletes the ``auth.currentUser`` and sign it out
  * @param mode whether the function is being used in default mode or in an automatic test
  * @param altAuth if the function is being used in an automatic test, it is necessary to pass the test ``auth`` instance
@@ -92,6 +123,6 @@ export async function deleteCurrentUser (mode:string, altAuth?:Auth) {
         return 'success'
     } catch (error) {
 
-        return 'failure'
+        return (error as any).code as string
     }
 }
