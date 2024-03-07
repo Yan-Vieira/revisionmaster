ddc02df8cedeadd67dbc0b530746e4a7589015d9
Yan Vieira <yanrei69@gmail.com>
2024-03-07 09:22:16 -0300

    fix: enum Auth atualizado

--- src/enums/Auth.ts
+++ src/enums/Auth.ts
@@ -3,7 +3,8 @@ const Auth = {
         argumentMissing: 'auth/argument-missing',
         invalidLoginMethod: 'auth/invalid-login-method',
         noUserSignedIn: 'auth/no-user-signed-in',
-        emailAlreadyInUse: 'auth/email-already-in-use'
+        emailAlreadyInUse: 'auth/email-already-in-use',
+        emailNotVerified: 'auth/email-not-verified'
     }
 }
 
